1ff87b6c0da81509f4de805e42d7e2c5c2f45751
Michael-Free <michael.d.free@gmail.com>
2021-10-09 00:01:19 -0400

    remote exec testing and clean up of wownero-node.tf

--- digitalocean/wownero-node.tf
+++ digitalocean/wownero-node.tf
@@ -11,41 +11,7 @@ resource "digitalocean_droplet" "Wownero-Node-Droplet" {
   size     = "s-1vcpu-1gb"
   region   = "nyc3"
   ssh_keys = ["${digitalocean_ssh_key.terraform.fingerprint}"]
-  provisioner "local-exec" {
-    command = "echo 'hello'"
+  provisioner "remote-exec" {
+    command = "echo HELLO"
   }
-  #connection {
-  #  type = "ssh"
-  #  user = "root"
-  #  private_key = "${file("~/.ssh/id_rsa")}"
-  #}
-  #provisioner "remote-exec" {
-  #  inline = [
-  #    "export PATH=$PATH:/usr/bin"
-  #  ]
-  #}
 }
-
-#  provisioner "remote-exec" {
-#    inline = [
-#      "export PATH=@$PATH:/usr/bin",
-#      "pwd"
-#    ]
-#  }
-#  connection {
-#    type = "ssh"
-#    user = "root"
-#    private_key="${file("~/.ssh/id_rsa)}"
-#  }
-#}
-
-#provisioner "remote-exec" {
-#  inline = [
-#    # declare the bash shell to be used
-#    "export PATH=$PATH:/usr/bin",
-#    # clone wownero-node-terraform repo using a bash command cause it's sloppy and fast. will update later
-#    "sudo git clone https://github.com/Michael-Free/wownero-node-terraform.git /root/wownero-node-terraform",
-#    "sudo chmod +x /root/wownero-node-terraform/install-remote/install-wownero.sh",
-#    "sudo ./install-wownero.sh"
-#  ]
-#}
