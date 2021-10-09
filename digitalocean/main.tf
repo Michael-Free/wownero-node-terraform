086cc35a9d6c67d3e0c8dd51d70cc11b3d8e31e2
Michael-Free <michael.d.free@gmail.com>
2021-10-09 11:12:21 -0400

    rename wownero-node.tf to main

---
+++ digitalocean/main.tf
@@ -0,0 +1,23 @@
+## Create a new ssh key
+resource "digitalocean_ssh_key" "terraform" {
+  name       = "Wownero-Node"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
+
+## Create a new Digital Ocean Droplet using the SSH key
+resource "digitalocean_droplet" "Wownero-Node-Droplet" {
+  name     = "Wownero-Node-Droplet"
+  image    = "ubuntu-20-04-x64"
+  size     = "s-1vcpu-1gb"
+  region   = "nyc3"
+  ssh_keys = ["${digitalocean_ssh_key.terraform.fingerprint}"]
+  connection {
+    type = "ssh"
+    host = self.ipv4_address
+    user = "root"
+    private_key = "${file("~/.ssh/id_rsa")}"
+  }
+  provisioner "remote-exec" {
+    inline = [ "echo hello" ]
+  }
+}
